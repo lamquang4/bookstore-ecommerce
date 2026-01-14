1dead0c89624822978d434865efd02adc98dab54
Quang lam <lamdieuquang0105@gmail.com>
2026-01-14 22:47:05 +0700

    cập nhật

--- frontend/src/components/client/Book/BookList.tsx
+++ frontend/src/components/client/Book/BookList.tsx
@@ -1,10 +1,11 @@
 import Image from "../../Image";
 import Loading from "../../Loading";
 import type { Book } from "../../../types/type";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import AdvancedSearch from "../AdvancedSearch";
 import { VscSettings } from "react-icons/vsc";
 import { useCallback, useState } from "react";
+import BookCard from "./BookCard";
 
 interface Props {
   category?: string;
@@ -12,6 +13,7 @@ interface Props {
   isLoading: boolean;
   total: number;
 }
+
 function BookList({ category, books, isLoading, total }: Props) {
   const location = useLocation();
   const navigate = useNavigate();
@@ -105,60 +107,7 @@ function BookList({ category, books, isLoading, total }: Props) {
         >
           {books.map((book) => {
             return (
-              <div className="space-y-[15px] rounded-sm" key={book.id}>
-                <div className="relative group">
-                  <Link to={`/book/${book.slug}`}>
-                    {book.images.length > 0 && (
-                      <div className=" w-full overflow-hidden pt-[100%]">
-                        <Image
-                          source={`${import.meta.env.VITE_BACKEND_URL}${
-                            book.images[0].image
-                          }`}
-                          alt={book.title}
-                          className="absolute inset-0 w-full h-full object-cover"
-                          loading="lazy"
-                        />
-                      </div>
-                    )}
-                  </Link>
-
-                  {book.discount > 0 && (
-                    <div className="absolute top-0 left-0 z-10 font-semibold text-center">
-                      <p className="text-white text-[0.8rem] px-2 py-1 bg-[#C62028] w-10 h-10 rounded-full text-center flex justify-center items-center">
-                        -{Math.floor((book.discount / book.price) * 100)}%
-                      </p>
-                    </div>
-                  )}
-
-                  {book.stock === 0 && (
-                    <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full text-center text-white p-1 font-medium bg-[#C62028] uppercase">
-                      Hết hàng
-                    </p>
-                  )}
-                </div>
-
-                <div className="space-y-[6px]">
-                  <h5 className="font-medium capitalize">{book.title}</h5>
-
-                  {book.discount > 0 ? (
-                    <div className="flex gap-[12px]">
-                      <del className="text-[#707072] text-[1rem]">
-                        {book.price.toLocaleString("vi-VN")}₫
-                      </del>
-
-                      <h5 className="font-semibold text-[#C62028]">
-                        {(book.price - book.discount).toLocaleString("vi-VN")}₫
-                      </h5>
-                    </div>
-                  ) : (
-                    <h5 className="font-semibold text-[#C62028]">
-                      {book.price.toLocaleString("vi-VN")}₫
-                    </h5>
-                  )}
-
-                  <p className="font-medium">{book.category.name}</p>
-                </div>
-              </div>
+           <BookCard book={book} key={book.id} />
             );
           })}
         </div>
---
+++ frontend/src/components/client/book/BookCard.tsx
@@ -0,0 +1,68 @@
+import { Link } from "react-router-dom";
+import type { Book } from "../../../types/type";
+import Image from "../../Image";
+
+interface Props {
+  book: Book;
+}
+
+function BookCard({ book }: Props) {
+  return (
+    <div className="space-y-[15px] rounded-sm" key={book.id}>
+      <div className="relative group">
+        <Link to={`/book/${book.slug}`}>
+          {book.images.length > 0 && (
+            <div className=" w-full overflow-hidden pt-[100%]">
+              <Image
+                source={`${import.meta.env.VITE_BACKEND_URL}${
+                  book.images[0].image
+                }`}
+                alt={book.title}
+                className="absolute inset-0 w-full h-full object-cover"
+                loading="lazy"
+              />
+            </div>
+          )}
+        </Link>
+
+        {book.discount > 0 && (
+          <div className="absolute top-0 left-0 z-10 font-semibold text-center">
+            <p className="text-white text-[0.8rem] px-2 py-1 bg-[#C62028] w-10 h-10 rounded-full text-center flex justify-center items-center">
+              -{Math.floor((book.discount / book.price) * 100)}%
+            </p>
+          </div>
+        )}
+
+        {book.stock === 0 && (
+          <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full text-center text-white p-1 font-medium bg-[#C62028] uppercase">
+            Hết hàng
+          </p>
+        )}
+      </div>
+
+      <div className="space-y-[6px]">
+        <h5 className="font-medium capitalize">{book.title}</h5>
+
+        {book.discount > 0 ? (
+          <div className="flex gap-[12px]">
+            <del className="text-[#707072] text-[1rem]">
+              {book.price.toLocaleString("vi-VN")}₫
+            </del>
+
+            <h5 className="font-semibold text-[#C62028]">
+              {(book.price - book.discount).toLocaleString("vi-VN")}₫
+            </h5>
+          </div>
+        ) : (
+          <h5 className="font-semibold text-[#C62028]">
+            {book.price.toLocaleString("vi-VN")}₫
+          </h5>
+        )}
+
+        <p className="font-medium">{book.category.name}</p>
+      </div>
+    </div>
+  );
+}
+
+export default BookCard;
